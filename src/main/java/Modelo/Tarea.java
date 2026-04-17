1037cf1f713a1a88b8bae0e3e8f3c8f868fe6506
Usuario25 <danieljimenezdelacruz3@gmail.com>
2026-04-17 14:30:40 +0200

    nnuevo

--- src/main/java/Modelo/Tarea.java
+++ src/main/java/Modelo/Tarea.java
@@ -1,4 +1,65 @@
-package Modelo;
+package model;
 
 public class Tarea {
+    private int id;
+    private String titulo;
+    private String estado;
+    private String prioridad;
+    private String responsable;
+
+    public Tarea(int id, String titulo, String estado, String prioridad, String responsable) {
+        this.id = id;
+        this.titulo = titulo;
+        this.estado = estado;
+        this.prioridad = prioridad;
+        this.responsable = responsable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getEstado() {
+        //LVelez
+        System.out.println("Manuel Morales");
+        return estado;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public boolean esCompletada() {
+        return "HECHA".equalsIgnoreCase(estado);
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + titulo + " (" + estado + ", " + prioridad + ", " + responsable + ")";
+    }
+
+    public void llamarPolicia(){
+
+    }
+
+    public void llamarbanco(){
+
+    }
+
 }
